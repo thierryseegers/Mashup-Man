193b2213b1a5926da4877cc30ffc0500984d512c
Thierry Seegers <surfintheusa@yahoo.com>
2021-10-19 17:12:47 +0200

    Introduce a background class
    This is responsible for drawing the background texture

---
+++ src/background.cpp
@@ -0,0 +1,20 @@
+#include "background.h"
+
+#include "resources.h"
+#include "scene.h"
+#include "sprite.h"
+
+#include <SFML/Graphics.hpp>
+
+background::background()
+    : sprite_{resources::textures().get(resources::texture::background)}
+{
+    sprite_.setColor({255, 255, 255, 255 / 6});
+}
+
+void background::draw_self(
+    sf::RenderTarget& target,
+    sf::RenderStates states) const
+{
+    target.draw(sprite_, states);
+}
