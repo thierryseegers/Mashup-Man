0e0cccfe90be9c785de38cf3d8e142c09fb36b3e
Thierry Seegers <surfintheusa@yahoo.com>
2021-11-01 13:46:41 +0100

    Register 'character_select' as a state
    Also, provide a helper header to include all states at once.

---
+++ src/state/states.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "state/character_select.h"
+#include "state/game_over.h"
+#include "state/game.h"
+#include "state/level_complete.h"
+#include "state/pause.h"
+#include "state/title.h"
